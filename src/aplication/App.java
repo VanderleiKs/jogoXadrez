362fcc8761b4b60bda32c885c51bc1da96f125e1
Vanderlei K Silva <vanderleiksilva@gmail.com>
2020-06-06 20:16:57 -0300

    Classe Position criada

---
+++ src/aplication/App.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import board.Position;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+		
+	}
+
+}
