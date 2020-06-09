48123653f5924a1a85fcc8e4a4b8668e69d256fb
Vanderlei K Silva <vanderleiksilva@gmail.com>
2020-06-09 11:31:58 -0300

    Criado método cleanConsole e tratamento de exceções

--- src/aplication/App.java
+++ src/aplication/App.java
@@ -1,7 +1,9 @@
 package aplication;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -15,18 +17,28 @@ public class App {
 		
 		
 		while(true) {
-		UI.printBoard(b.getPiece());
-		System.out.println();
-		System.out.print("Source: ");
-		ChessPosition source = UI.readChessPosition(scan);
-		
-		System.out.println();
-		System.out.print("Target: ");
-		ChessPosition target = UI.readChessPosition(scan);
-		
-		ChessPiece capturedPiece = b.performChessMove(source, target);
+			try {
+				UI.clearScreen();	
+				UI.printBoard(b.getPiece());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(scan);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(scan);
+				
+				ChessPiece capturedPiece = b.performChessMove(source, target);
 
-		
+			}
+			catch(ChessException ex){
+				System.out.println(ex.getMessage());
+				scan.nextLine();
+			}
+			catch(InputMismatchException ex) {
+				System.out.println(ex.getMessage());
+				scan.nextLine();
+			}
 		}
 	}
 
