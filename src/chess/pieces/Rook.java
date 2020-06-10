0cdd4695d7ba5d483e38ea5b29b8329e3df4c8d3
Vanderlei K Silva <vanderleiksilva@gmail.com>
2020-06-10 20:25:04 -0300

    Adicionada a classe Bispo

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -11,11 +11,6 @@ public class Rook extends ChessPiece {
 	public Rook(Board board, Color color) {
 		super(board, color);
 	}
-	
-	@Override
-	public String toString() {
-		return "R";
-	}
 
 	@Override
 	public boolean[][] possibleMoves() {
@@ -68,6 +63,11 @@ public class Rook extends ChessPiece {
 		}
 		
 		return mat;
+	}
+	
+	@Override
+	public String toString() {
+		return "R";
 	}
 	
 }
